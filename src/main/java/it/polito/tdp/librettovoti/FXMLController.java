a1c92c288f6854a3e1844032293a78d9d2ae7a91
ludov <ludov@LAPTOP-8O3J38D4>
2021-03-15 10:32:37 +0100

    MVC inizio

--- src/main/java/it/polito/tdp/librettovoti/FXMLController.java
+++ src/main/java/it/polito/tdp/librettovoti/FXMLController.java
@@ -1,25 +1,81 @@
 package it.polito.tdp.librettovoti;
 
+import java.awt.Button;
+import java.awt.event.ActionEvent;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 
-public class FXMLController implements Initializable {
+import it.polito.tdp.librettovoti.model.Libretto;
+import it.polito.tdp.librettovoti.model.Voto;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class FXMLController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtNomeEsame;
+
+    private Libretto model;
     
     @FXML
-    private Label label;
+    private TextField txtVoto;
+
+    @FXML
+    private Button btInserisci;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private TextField txtData;
+
+    @FXML
+    private TextArea txtResult;
+
+    public void setModel(Libretto model) {
+    	this.model = model;
     }
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    @FXML
+    void handleInserisci(ActionEvent event) {
+    	//controllo i dati
+    	String nomeEsame = txtNomeEsame.getText();
+    	if(nomeEsame.length() == 0) {
+    		txtResult.setText("ERRORE: nome Esame vuoto");
+    		return;
+    	}
+    	
+    	String votoEsame = txtVoto.getText();
+    	String dataEsame = txtData.getText();
+    	int votoInt;
+    	try {
+    	   votoInt = Integer.parseInt(votoEsame);
+    	}catch(NumberFormatException nfe) {
+    		txtResult.setText("ERRORE: voto deve essere un numero intero");
+    		return;
+    	}
+    	LocalDate data = LocalDate.parse(dataEsame);
+    	
+    	//esegui l'azione
+    	Voto voto = new Voto(nomeEsame, votoInt, data);
+    	model.add(voto);
+    	
+    	//Aggiorna i risultati nella view
+    	txtResult.setText(model.toString());
+    }
+    
+    @FXML
+    void initialize() {
+        assert txtNomeEsame != null : "fx:id=\"txtNomeEsame\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtVoto != null : "fx:id=\"txtVoto\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtData != null : "fx:id=\"txtData\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btInserisci != null : "fx:id=\"btInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
 }
